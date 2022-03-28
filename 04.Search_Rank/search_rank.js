6e7490ebaf3851e7db5a1d4eee37e11decb9c646
woody-park <starface1009@gmail.com>
2022-03-29 05:37:36 +0900

    정확성 통과 효율성 테스트 실패

--- 04.Search_Rank/search_rank.js
+++ 04.Search_Rank/search_rank.js
@@ -1,37 +1,35 @@
 /**
+ * TODO: 
+ *   
+ *   - query문을 기준으로(조건)  info데이터와 조회하여 결과 추출
+ *   - query[0] 번에 대한 질문을 => info 전체 데이터 내에서 검색 => 결과는 []으로
+ *   -  '-' 키워드가 존재하면 모든 조건을 통과할 것 
+ *  // - 카운트를 활용하여 조건을 통과하면 누적시켜 일정 숫자에 도달할 경우 각 배열의 마지막 값인 '점수'를 비교하여 최종 함격자 판별 =>[카운트 제거]
+ *   - 조건을 통과하면 true, 아니면 false를 배열에 저장하여 각 조건에 맞는 지원자 판별
+ *   - 최대한 메서드 활용하기 
+ *   - 코드의 길이 최대한 줄이기 
+ *   - 무의미한 반복된 코드 줄이기
  * 
-  TODO:  query문을 기준으로(조건)  info데이터와 조회하여 결과 추출
-          '-' 키워드가 존재하면 모든 조건을 통과할 것 
-
-          query[0] 번에 대한 질문을 => info 전체 데이터 내에서 검색 => 결과는 []으로
+ *   ! 주석 등 지저분한 것들 정리
  */
 
-//1 . 언어 [자바, 파이썬, cpp]
-//2 . 직군 [프런트, 백엔드]
-//3 . 경력 [주니어, 시니어]
-//4 . 푸드 [피자, 치킨]
-//5 . 점수 [1점 ~ 10만점 이하]
-
-
-// [1,2,3,4,5] => 1번이면 => 1[1,2,3], 2번이면 => [1,2]
-
 /**
- * 
  * FIXME: 
  * 
- * 1. 검색조건에 따른 합격자 배열 숫자 누적 방지
- * 2. 3번째 검색 조건에 5번째 지원자 정보 맞추는 부분에서 카운트 누적이 4가 되지만, 점수 조건이 안되는 경우인데 합격 되는 경우 
- * 3. 문자열인 숫자 -> 숫자형으로 변환하기 [정학한 판단이 되지 않음] => parseInt
+ * [헤결] 1. 검색조건에 따른 합격자 배열 숫자 누적 방지
+ * [해결] 2. 3번째 검색 조건에 5번째 지원자 정보 맞추는 부분에서 카운트 누적이 4가 되지만, 점수 조건이 안되는 경우인데 합격 되는 경우 
+ * [해결] 3. 문자열인 숫자 -> 숫자형으로 변환하기 [정학한 판단이 되지 않음] => parseInt
+ *      
  *  */
 
 
-
-
+// ? 누적된 배열 안의 true의 갯수 판단하여 4개 이상일 경우 true를 반환해주는 함수
 const judgeCountTrue = function (box) {
-    let judge = box.reduce((a, c) => {
-        a[c] = (a[c] || 0) + 1
-        return a;
+    let judge = box.reduce((acc, curValue) => {
+        acc[curValue] = (acc[curValue] || 0) + 1
+        return acc;
     }, {});
+    // true의 갯수만 활용할 것이므로 judge자체는 리턴할 필요없다
     //return judge;
 
     let weWant = (judge['true'] === 4);
@@ -39,39 +37,15 @@ const judgeCountTrue = function (box) {
 }
 
 
-
-
-
-
-
-
-
 function solution(info, query) {
 
-    let infoArr, queryArr, queryValueSpace, inputQueryArr;
-    let count = 0;
-    var people = 0;
+    let infoArr, queryArr, queryValueSpace, inputQueryArr, answer, queryInfo, infoInfo;
+    let people = 0;
     let arr = [];
-    let answer;
-    let savePeople = [];
     let savePoint = [];
-    let queryInfo;
-    let infoInfo;
-
-    // console.log(info);
-    //console.log(query.length);
-
-    // console.log(query[0].split(' and '));
-    // console.log(info[0].split(' '));
-
 
     for (let j = 0; j < query.length; j++) {
-        console.log('==========================================');
-        console.log('==========================================');
-        people = 0;
-        console.log("현재 저장된 피플", savePeople);
-        savePeople = [];
-        arr = [];
+
         for (let i = 0; i < info.length; i++) {
             //count = 0;
             infoArr = info[i].split(' '); // [ 'java', 'backend', 'junior', 'pizza', '150' ]
@@ -87,115 +61,49 @@ function solution(info, query) {
             // 지원자 정보
             infoInfo = infoArr;  // [ 'java', 'backend', 'junior', 'pizza', '150' ]
 
-            console.log('==========================================');
-            console.log('==========================================');
-            console.log(`${j}번째 검색 조건: `, queryInfo);
-            console.log(`${i}번째 지원자 정보: `, infoInfo);
-
-
             queryInfo.forEach((element, index) => {
+                // 누적된 arr의 상태 판별 현재 ture가 4개 이상인지 
+                let figureIt = judgeCountTrue(arr);
 
-                console.log(`조건${index}번째`, element);
-
-                //console.log(infoInfo[index]);
-                //console.log(element === infoInfo[index]);
-                //console.log(queryInfo.includes(infoInfo[index], 0));
-
+                // 조건의 element가 '-'이면 queryInfo 배열의 해당 value를 지원자 배열의 해당 value 값으로 변경하여 일치시킨다.
                 if ((element === '-')) {
                     queryInfo[index] = infoInfo[index];
-                    //console.log("변경된 queryInfo", queryInfo);
                 }
 
+                // index는 4개 이상이 되면 안된다. 넘어가면 판별이 누적됨..
                 if ((0 <= index) && (index < 4)) {
+                    // 요구조건안에 지원자의 조건과 일치하면 true를 아니면 false를 arr에 넣는다.
                     arr.push(queryInfo.includes(infoInfo[index], 0));
-                    console.log("현재", arr);
                 }
 
-                let figureIt = judgeCountTrue(arr);
-                console.log("현재 ture의 갯수가 4개인가", figureIt);
-
-
-                if ((arr[index] === true)) {
-                    //console.log('통과');
-                    //count++
-                    //console.log("카운트", count);
-                    // }
-
-                    // if (arr[index] === true) {
-                    //     console.log('매칭으로 통과');
-                    //     count++
-                    //     console.log(count);
-
-
-
-                    if (((figureIt) && (parseInt(infoInfo[4], 10) >= parseInt(queryInfo[4], 10)))) {
-                        // console.log('모든 조건 완료');
-                        //count = 0;
-                        people++;
-                        console.log("검색된 지원자", people + '명');
-                        arr = [];
-                        savePeople.push(people);
-
-                        console.log("현재 저장된 피플", savePeople);
-
-                    }
-
-
-
+                // true가 4개인 경우 => 점수 판별하여 통과하면 people 카운트 +1 한다
+                if (((figureIt) && (parseInt(infoInfo[4], 10) >= parseInt(queryInfo[4], 10)))) {
+                    people++;
+                    arr = [];
                 }
-                //console.log(info.length);//6
+
+
+                // 지원자가 조건을 통과하거나 못하거나 
+                // index가 4일 경우 즉, 점수 판별을 한 경우 다음 지원자 판별을 위해 arr를 다시 초기화 한다 
                 if (index === 4) {
-                    console.log("카운트", count);
-                    //count = 0;
                     arr = [];
                 }
-
-                if (arr.length >= 5) {
-                    console.log("길이 한도", arr);
-                    arr = [];
-
-                }
-
-                //console.log('사람수', people);
-
-
             });
 
-
         }
+        // 각 질문에 대한 지원자를 판별하고 누적된 people을 배열에 넣는다
         savePoint.push(people);
-        console.log('저장갯수', savePoint);
+
+        // 새로운 질문 이전에 people을 초기화한다 
+        people = 0;
     }
 
-
-
-
-    // console.log("저장된 합격자 수", savePeople);
-
-    // answer.push(savePeople.length);
-
-
     answer = savePoint;
-
-    console.log("최종전", savePoint);
-    console.log('==========================================');
-    console.log('==========================================');
+    console.log("각 조건의 최종 합격자 명단", savePoint);
 
     return answer;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let info =
